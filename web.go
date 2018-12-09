9684d4bac60bd005a9d99cbf4398939b01d4a262
Arseny Akinfiev <arseny.jin@ya.ru>
2018-12-09 08:00:40 +0800

    ADD: go server

--- web.go
+++ web.go
@@ -9,7 +9,7 @@ import (
 // ShowFile - display html file
 func ShowFile(w http.ResponseWriter, fileName string) {
 	var htmlString string
-	htmlBytes, err := ioutil.ReadFile(fileName)
+	htmlBytes, err := ioutil.ReadFile("" + fileName)
 	if err != nil {
 		htmlString = "File Not Found : " + fileName
 	} else {
@@ -24,18 +24,18 @@ func test(w http.ResponseWriter, r *http.Request) {
 // func test2(w http.ResponseWriter, r *http.Request) {
 // 	ShowFile(w, "d4peep2.html")
 // }
-//
-// func js(w http.ResponseWriter, r *http.Request) {
-// 	ShowFile(w, "web3.min.js")
-// }
+
+func js(w http.ResponseWriter, r *http.Request) {
+	ShowFile(w, "web3.min.js")
+}
 
 func main() {
 	http.Handle("/inc/", http.StripPrefix("/inc/", http.FileServer(http.Dir("inc"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 	http.HandleFunc("/", test)
-	http.HandleFunc("/2", test2)
+	// http.HandleFunc("/2", test2)
 
-	if err := http.ListenAndServe(":9092", nil); err != nil {
+	if err := http.ListenAndServe(":9091", nil); err != nil {
 		fmt.Println(err)
 	}
 }
